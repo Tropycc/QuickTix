1dc38ade36e38a785d60c5091397d3b566c9176f
visua <visua@VISUALSTUDIOVM>
2025-09-17 11:34:37 -0400

    Add project files.

---
+++ QuickTix/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace QuickTix.Models
+{
+    public class Event
+    {
+        //Primary Key
+        public int EventId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Owner { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+   
+    }
+}
---
+++ QuickTix/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿namespace QuickTix.Models
+{
+    public class Ticket
+    {
+        //Primary Key
+        public int TicketId { get; set; }
+        //Foreign Key
+        public int EventId { get; set; }
+
+        //Ticket Owner
+        public string Owner { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        //Navigation Property
+        public List <Event>? Events { get; set; }
+
+    }
+}
