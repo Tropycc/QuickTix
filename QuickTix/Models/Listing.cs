a9316ab42358b55c9f9f14b7315735769edebec9
Tropycc <W0511519@nscc.ca>
2025-10-03 17:13:42 -0400

    Final Assignment 1 Changes

--- QuickTix/Models/Listing.cs
+++ QuickTix/Models/Listing.cs
@@ -1,4 +1,6 @@
-﻿namespace QuickTix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickTix.Models
 {
     public class Listing
     {
@@ -6,13 +8,21 @@
         public int ListingId { get; set; }
 
         //Foreign Keys
+
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
+
+        [Display(Name = "Owner Number")]
         public int OwnerId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
+
+        [Display(Name = "Owner Name")]
         public string Owner { get; set; } = string.Empty;
+
+        [Display(Name = "Listing Date")]
         public DateTime ListingDate { get; set; }
 
         //Navigation Properties
